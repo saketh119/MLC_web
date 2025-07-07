b2dcfe9af9e3576c9fe533a295e73f59e67078c9
Your Name <asaketh12@gmai.com>
2025-07-07 22:41:09 +0530

    preprod2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // any other config options
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mlcevents.s3.eu-north-1.amazonaws.com',
+      },
+    ],
+  },
+}
 
-module.exports = nextConfig; // ✅ CommonJS export
+module.exports = nextConfig;
